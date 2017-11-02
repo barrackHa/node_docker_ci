993f361b1f0254cc38a9e1c18681c35b71412119
Barak Haim <Barak.Haim@mail.tau.ac.il>
2017-11-02 16:50:01 +0200

    minor docker script upgrade

--- .travis/dockerizer.sh
+++ .travis/dockerizer.sh
@@ -3,7 +3,7 @@
 docker build -t barakh1985/barakci .
 docker run -d -p 8080:8080 barakh1985/barakci
 docker ps
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-	docker push USER/REPO;
-fi
+#if [ "$TRAVIS_BRANCH" == "master" ]; then
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker push barakh1985/barakci;
+#fi
