b100a22e4ec99e000a10973fc4d1ae8d4d41b48d
Barak Haim <Barak.Haim@mail.tau.ac.il>
2017-11-02 16:43:52 +0200

    refactored docker cmd to external script. run only on node 6

---
+++ .travis/dockerizer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build -t barakh1985/barakci .
+docker run -d -p 8080:8080 barakh1985/barakci
+docker ps
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+	docker push USER/REPO;
+fi
