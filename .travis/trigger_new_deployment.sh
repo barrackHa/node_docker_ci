9e40d03d3adf8fd9e1dc52986a765b529ffb0b5b
Barak Haim <Barak.Haim@mail.tau.ac.il>
2017-11-03 16:50:41 +0200

    solving permission denied

--- .travis/trigger_new_deployment.sh
+++ .travis/trigger_new_deployment.sh
@@ -8,6 +8,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
+	chmod +x ./*
     ./ecs-deploy.sh -c $CLUSTER_NAME -n $SERVICE -i barakh1985/barakci
   else
     echo "Skipping deploy because it's not an allowed branch"
