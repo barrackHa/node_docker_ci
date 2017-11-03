c2719dbd2ec53d3101c2991adbe10958536ab960
Barak Haim <Barak.Haim@mail.tau.ac.il>
2017-11-03 16:32:59 +0200

    added the deployment scripts to travis and updated index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Docker-Node 11th\n');
+  res.send('Hello Docker-Node 99th\n');
 });
 
 app.listen(PORT);
