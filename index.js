6ce276b68a8dabb5621a07616430c1492eabb071
Barak Haim <Barak.Haim@mail.tau.ac.il>
2017-11-04 15:23:47 +0200

    updated index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Docker-Node 100th\n');
+  res.send('Hello Docker-Node 200th\n');
 });
 
 app.listen(PORT);
